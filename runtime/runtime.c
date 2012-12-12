a44e2ee1299cd05803297999dd15339ddd073d3f
Rasmus Svensson <raek@raek.se>
2012-12-12 18:58:32 +0100

    Spränga isär runtime.c

--- runtime/runtime.c
+++ runtime/runtime.c
@@ -1,76 +1,6 @@
 #include <stdint.h>
 #include <stdlib.h> // malloc
-
-struct Call;
-struct Env;
-struct Args;
-
-typedef void(*FnFree)(void*);
-typedef struct Call *(*FnSpec)(struct Env*, struct Args*);
-
-typedef struct {
-  uint16_t count;
-  FnFree free_fn;
-} RefCount;
-
-typedef struct {
-  RefCount ref_count;
-} RefCountable;
-
-typedef struct {
-  char *name;
-  RefCountable *value;
-} Pair;
-
-typedef struct _Env {
-  RefCount ref_count;
-  uint16_t count;
-  Pair *bindings;
-  struct _Env *parent;
-} Env;
-
-typedef struct {
-  RefCount ref_count;
-  uint16_t count;
-  RefCountable **args;
-} Args;
-
-typedef struct {
-  RefCount ref_count;
-  int16_t value;
-} Number;
-
-typedef struct {
-  RefCount ref_count;
-  FnSpec fn_spec;
-  Env *env;
-} Closure;
-
-typedef struct {
-  RefCount ref_count;
-  Closure *closure;
-  Args *args;
-} Call;
-
-void init_ref_count(RefCount *ref_count, FnFree free_fn);
-void ref_count_acquire(RefCountable *ref_count);
-void ref_count_release(RefCountable *ref_count);
-
-Env *create_env(Env *parent);
-void env_insert(Env *env, char *key, RefCountable *ref_countable);
-void *env_lookup(Env *env, char *key);
-void free_env(void *env);
-
-Args *create_args(int size);
-void args_set(Args *args, int i, RefCountable *ref_countable);
-void *args_get(Args *args, int i);
-void free_args(void *args);
-
-Number *create_number(int i);
-void free_number(void *number);
-
-Closure *create_closure(FnSpec fn_spec, Env env);
-void free_closure(void *closure);
+#include "runtime.h"
 
 void init_ref_count(RefCount *ref_count, FnFree free_fn)
 {
@@ -128,7 +58,39 @@ void free_env(void *ptr)
   free(env);
 }
 
+void env_insert(Env *env, char *key, RefCountable *ref_countable)
+{
+  Pair p;
 
+  env->count += 1;
+  env->bindings = realloc(env->bindings, env->count * sizeof(*env->bindings));
+  p.name = key;
+  p.value = ref_countable;
+  env->bindings[env->count - 1] = p;
+  ref_count_acquire(ref_countable);
+}
+
+void *env_lookup(Env *env, char *key)
+{
+  Env *currentEnv;
+  int i;
+
+  currentEnv = env;
+  do
+    {
+      for (i = 0; i < currentEnv->count; i += 1)
+        {
+          if (strcmp(currentEnv->bindings[i].name, key) == 0)
+            {
+              return currentEnv->bindings[i].value;
+            }
+        }
+      currentEnv = currentEnv->parent;
+    }
+  while (currentEnv != NULL);
+
+  return NULL;
+}
 
 
 
